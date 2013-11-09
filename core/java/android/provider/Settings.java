e06bf91bd6bb8b995906857b359a8c309108ab54
Lars Greiss <kufikugel@googlemail.com>
2013-11-09 18:11:27 +0100

    fb: add back all keyboard IME features back (1/2)
    this commit adds:
    
    - ime switcher notification - thanks cm
    - slims fullscreen disable on landscape
    - slims force autorotation on IME
    - volume button control for navigate in the message - thanks cm
    - slims force to show always enter key
    
    some fixes to the 4.2/4.3 implementations + added full multiuser
    support for all this features
    
    cheers
    
    Change-Id: I71e93c39620cc171e658b2d7e26491bac97c651d
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	core/java/android/inputmethodservice/InputMethodService.java
    	core/java/android/provider/Settings.java
    	services/core/java/com/android/server/InputMethodManagerService.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3078,6 +3078,47 @@ public final class Settings {
          * @hide
          */
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
+        
+         /**
+          * Volume keys control cursor in text fields (default is 0)
+          * 0 - Disabled
+          * 1 - Volume up/down moves cursor left/right
+          * 2 - Volume up/down moves cursor right/left
+          * @hide
+          */
+         public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+
+        /**
+         * Override and forcefully disable the fullscreen keyboard
+         * @hide
+         */
+        public static final String DISABLE_FULLSCREEN_KEYBOARD = "disable_fullscreen_keyboard";
+
+        /**
+         * Whether to show the IME switcher in the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_IME_SWITCHER = "status_bar_ime_switcher";
+
+        /**
+         * Automatic keyboard rotation timeout.  0 to disable completely.
+         * @hide
+         */
+        public static final String KEYBOARD_ROTATION_TIMEOUT = "keyboard_rotation_timeout";
+
+        /**
+         * Forces formal text input.  1 to replace emoticon key with enter key.
+         * @hide
+         */
+        public static final String FORMAL_TEXT_INPUT = "formal_text_input";
+        
+         /**
+         * Volume rocker wake
+         * @hide
+         */
+        public static final String VOLUME_WAKE_SCREEN = "volume_wake_screen";
+
+
 
         /**
           * Volume keys control cursor in text fields (default is 0)
@@ -3313,6 +3354,13 @@ public final class Settings {
         @Deprecated
         public static final String WIFI_NETWORKS_AVAILABLE_NOTIFICATION_ON =
                 Global.WIFI_NETWORKS_AVAILABLE_NOTIFICATION_ON;
+                
+         /**
+         * wake up when plugged or unplugged
+         *
+         * @hide
+         */
+        public static final String WAKEUP_WHEN_PLUGGED_UNPLUGGED = "wakeup_when_plugged_unplugged";
 
         /**
          * @deprecated Use
--- services/core/java/com/android/server/InputMethodManagerService.java
+++ services/core/java/com/android/server/InputMethodManagerService.java
@@ -419,9 +419,10 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
                     Settings.System.STATUS_BAR_IME_SWITCHER),
                     false, new ContentObserver(mHandler) {
                         public void onChange(boolean selfChange) {
-                            updateFromSettingsLocked(true);
+
+                            updateInputMethodsFromSettingsLocked(true);
                         }
-                    });
+                    }, UserHandle.USER_ALL);
         }
 
         @Override public void onChange(boolean selfChange, Uri uri) {
@@ -1735,14 +1736,17 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
             mCurMethodId = null;
             unbindCurrentMethodLocked(true, false);
         }
-        // code to disable the CM Phone IME switcher with config_show_cmIMESwitcher set = false
+
+        // code to disable the IME switcher with config_show_IMESwitcher set = false
         try {
-            mShowOngoingImeSwitcherForPhones = Settings.System.getInt(mContext.getContentResolver(),
-            Settings.System.STATUS_BAR_IME_SWITCHER) == 1;
+            mShowOngoingImeSwitcherForPhones =
+                Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_IME_SWITCHER, UserHandle.USER_CURRENT) == 1;
         } catch (SettingNotFoundException e) {
             mShowOngoingImeSwitcherForPhones = mRes.getBoolean(
-            com.android.internal.R.bool.config_show_cmIMESwitcher);
+                com.android.internal.R.bool.config_show_IMESwitcher);
         }
+
         // Here is not the perfect place to reset the switching controller. Ideally
         // mSwitchingController and mSettings should be able to share the same state.
         // TODO: Make sure that mSwitchingController and mSettings are sharing the
