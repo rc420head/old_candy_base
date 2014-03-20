da4fe30c1a1cffe7edc1a3a51cff94d35674eaad
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2014-03-20 20:48:04 +0700

    [1/2] Base:  circle ribbon -seperated from slim_strings and created candy_strings
    add whitelist app to include into circle view
    rebased
    make standalone patch
    
    PS.15 : rebased
    PS.16 : cleanup
    
    Change-Id: I08d2a3a7e0f8df20e801e585d4cac8547a740a3e
    
    Conflicts:
    	packages/SystemUI/res/values/slim_strings.xml
    
     Author:    Yanuar Harry <ai.the.smarties.physics@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1826,6 +1826,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             }
         } else if (!mRecreating) {
             // usual case: status bar visible & not immersive
+            addAppCircleSidebar();
 
             // show the ticker if there isn't already a heads up
             if (mInterruptingNotificationEntry == null) {
