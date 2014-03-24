7eacd9a8199bd901210f3aaf522128c1fb28d034
Raju Yadav <raju.yadav@sonymobile.com>
2014-03-24 10:44:00 +0100

    Avoid SystemUI crash due to NPE
    BaseStatusBar.createNotificationViews() returns null
    when it reports handleNotificationError(), leading to
    a forced crash of the misbehaving app. However, it
    should not add a null object of Notification.Entry
    since it will also crash SystemUI (crashing at compare).
    
    Change-Id: I86678c8024e931420b0fbab1f36b330e7e75d581

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -1239,6 +1239,9 @@ public abstract class BaseStatusBar extends SystemUI implements
     }
 
     protected void addNotificationViews(NotificationData.Entry entry) {
+        if (entry == null) {
+            return;
+        }
         // Add the expanded view and icon.
         int pos = mNotificationData.add(entry);
         if (DEBUG) {
