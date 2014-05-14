3a3a3c17878e37519a7d784263c4ed46919d185d
errantzebra845 <errantzebra845@gmail.com>
2014-05-14 01:04:56 -0400

    TRDSNonintrusive 4/4

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3601,6 +3601,13 @@ public final class Settings {
         public static final String CALL_FLIP_ACTION_KEY = "call_flip_action_key";
 
         /**
+         * Should the non-intrsive incall ui be used
+         *
+         * @hide
+         */
+        public static final String NON_INTRUSIVE_INCALL = "non_intrusive_incall";
+
+        /**
          * Enable Stylus Gestures
          *
          * @hide
