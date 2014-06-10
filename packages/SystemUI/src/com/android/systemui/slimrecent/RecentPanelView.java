403dc3f7dbe39cb4c129eef3ddc4069689b88726
Schubi <schubi@erlangen.ccc.de>
2014-06-11 00:35:48 +0200

    Slim Recents: Always show topmost task collapsed
    PS2: Also restore expanded state of topmost task when on homescreen
    PS3: Fix code style
    
    Change-Id: I64956410fb389d8e880979a84b51321d8b372217

--- packages/SystemUI/src/com/android/systemui/slimrecent/RecentPanelView.java
+++ packages/SystemUI/src/com/android/systemui/slimrecent/RecentPanelView.java
@@ -715,6 +715,7 @@ public class RecentPanelView {
                     // Save the first task for later use.
                     mFirstTask = item;
                 }
+
                 if (i > 0 || showTopmost) {
                     // FirstExpandedItems value forces to show always the app screenshot
                     // if the old state is not known and the user has set expanded mode to auto.
