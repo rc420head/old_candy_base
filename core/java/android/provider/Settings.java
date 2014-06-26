cf58598425ab4b4f7114945d0baa1b022eec2ab5
dankoman <dankoman30@gmail.com>
2014-06-26 02:53:30 +0200

    FB: Slimrecents background color && transparency [2/2]
    Change-Id: Icf0259ce45d416364d6e7a7a8c2038f6c4c0cbde
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
     Author:    dankoman <dankoman30@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4261,6 +4261,13 @@ public final class Settings {
         public static final String HOVER_LONG_FADE_OUT_DELAY = "hover_long_fade_out_delay";
 
         /**
+         * Recent panel background color
+         *
+         * @hide
+         */
+        public static final String RECENT_PANEL_BG_COLOR = "recent_panel_bg_color";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
