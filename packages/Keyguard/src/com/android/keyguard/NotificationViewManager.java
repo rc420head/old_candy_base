d2e74a982ce11954bef4913ef3a49d4770d78f41
Jubakuba <Jubakuba@gmail.com>
2014-06-30 11:59:58 -0600

    Framework: Rewrite Quiet Hours (2/3)
    Added:
    	New Toggle States.
    		Toggle is now aware
    		of if quiet hours
    		is truly enabled or not.
    		Enable "force" mode
    		on click: turns quiet hours
    		on / off
    		Double-click to return
    		to "timed" mode.
    
    	New Features:
    		Wifi Connected Requirement
    		Battery Charging Requirement
    		Individual day times
    
            Completely rewrote quiet hours
                    to reduce system calls.
                    No more calendar objects
                    or checking three preferences
                    and comparing their values
                    on each quiet hours check.
                    Reduce that to ONE preference
                    check per preference
    
    PS2: Ensure booted before firing intent
    
    Change-Id: I61ef5afa32c5ff883129b9362423af7f724c5ac1
    
    Conflicts:
    	core/java/com/android/internal/util/slim/QuietHoursHelper.java
    	packages/SystemUI/src/com/android/systemui/ReminderReceiver.java
    	packages/SystemUI/src/com/android/systemui/quicksettings/QuietHoursTile.java
    	services/java/com/android/server/BatteryService.java
    	services/java/com/android/server/NotificationManagerService.java
    	services/java/com/android/server/VibratorService.java
    
     Author:    Jubakuba <Jubakuba@gmail.com>

--- packages/Keyguard/src/com/android/keyguard/NotificationViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/NotificationViewManager.java
@@ -41,8 +41,6 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-import com.android.internal.util.slim.QuietHoursHelper;
-
 public class NotificationViewManager {
     private final static String TAG = "Keyguard:NotificationViewManager";
 
@@ -163,7 +161,8 @@ public class NotificationViewManager {
                     if (event.values[0] >= ProximitySensor.getMaximumRange()) {
                         if (config.pocketMode && mTimeCovered != 0 && (config.showAlways || mHostView.getNotificationCount() > 0)
                                 && System.currentTimeMillis() - mTimeCovered > MIN_TIME_COVERED
-                                && !QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_DIM)) {
+                                && Settings.System.getInt(mContext.getContentResolver(),
+                                Settings.System.QUIET_HOURS_DIM, 0) == 2) {
                             wakeDevice();
                             mWokenByPocketMode = true;
                             mHostView.showAllNotifications();
@@ -192,7 +191,8 @@ public class NotificationViewManager {
                     config.forceExpandedView);
             if (added && config.wakeOnNotification && screenOffAndNotCovered
                       && showNotification && mTimeCovered == 0
-                      && !QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_DIM)) {
+                      && Settings.System.getInt(mContext.getContentResolver(),
+                      Settings.System.QUIET_HOURS_DIM, 0) == 2) {
                 wakeDevice();
             }
         }
