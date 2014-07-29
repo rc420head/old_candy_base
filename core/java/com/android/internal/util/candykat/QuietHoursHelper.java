68754d4efdd035799345d4b573f1e8d5f248fc43
errantzebra845 <errantzebra845@gmail.com>
2014-07-29 13:11:28 -0400

    bring in line with candyrom source -thanks liquid

--- core/java/com/android/internal/util/candykat/QuietHoursHelper.java
+++ core/java/com/android/internal/util/candykat/QuietHoursHelper.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.internal.util.liquid;
+package com.android.internal.util.candykat;
 
 import android.content.Context;
 import android.os.UserHandle;
