0d82371cdc312731549bae32820647c1c48447f7
rc420head <dennislebhardt@yahoo.com>
2014-12-04 10:13:03 -0600

    now settings builds

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3108,6 +3108,12 @@ public final class Settings {
          */
         public static final String DISABLE_FULLSCREEN_KEYBOARD = "disable_fullscreen_keyboard";
 
+ 
+        /**
+         * Automatic keyboard rotation timeout.  0 to disable completely.
+         * @hide
+         */
+        public static final String KEYBOARD_ROTATION_TIMEOUT = "keyboard_rotation_timeout";
 
         /**
          * Forces formal text input.  1 to replace emoticon key with enter key.
